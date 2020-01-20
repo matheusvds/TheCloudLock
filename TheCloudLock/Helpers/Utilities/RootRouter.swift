207818b1a4f294b17e1fb3287124b264b352fa1f
Matheus Vasconcelos <>
2020-01-20 12:07:01 -0300

    refactors how keyWindow is found due to obsoletion in iOS 13.0

--- TheCloudLock/Helpers/Utilities/RootRouter.swift
+++ TheCloudLock/Helpers/Utilities/RootRouter.swift
@@ -12,7 +12,7 @@ class RootRouter {
     /** Replaces root view controller. You can specify the replacment animation type.
      If no animation type is specified, there is no animation */
     func setRootViewController(controller: UIViewController, animatedWithOptions: UIView.AnimationOptions?) {
-        guard let window = UIApplication.shared.keyWindow else {
+        guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else {
             fatalError("No window in app")
         }
         if let animationOptions = animatedWithOptions, window.rootViewController != nil {
