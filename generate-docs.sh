8b796f53734dfef1f3f3012abffa3d98ebdcf113
Matheus Vasconcelos <>
2020-01-28 11:41:09 -0300

    updates documentation generation script to save in a different folder name

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,1 +1,1 @@
-bundle exec jazzy --min-acl internal --no-hide-documentation-coverage --theme fullwidth --output ./docs && rm -rf ./build
+bundle exec jazzy --min-acl internal --no-hide-documentation-coverage --theme fullwidth --output ./documentation && rm -rf ./build
